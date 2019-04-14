629bb604b233c1d568376fc781c57a6300328cdb
d9394 <9394952@gmail.com>
2019-04-14 09:47:26 +0800

    Update pymultimonaprs.lua

--- luasrc/controller/pymultimonaprs.lua
+++ luasrc/controller/pymultimonaprs.lua
@@ -2,7 +2,7 @@ module("luci.controller.pymultimonaprs", package.seeall)
 
 function index()
         if not nixio.fs.access("/etc/config/pymultimonaprs") then
-                nixio.fs.writefile("/etc/config/pymultimonaprs","config pymultimonaprs\nconfig rtl\nconfig alsa\nconfig beacon\n")
+                nixio.fs.writefile("/etc/config/pymultimonaprs","config pymultimonaprs main\n")
         end
 
         local page=entry({"admin", "services", "pymultimonaprs"}, cbi("pymultimonaprs"), _("pymultimonaprs"))
